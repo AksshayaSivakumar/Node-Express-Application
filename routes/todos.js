7150167ff355889ba9ad192c4e6d19574e2e5814
AksshayaSivakumar <aksshayapugal@gmail.com>
2024-05-21 13:49:45 +0530

    Node Express

---
+++ routes/todos.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const todos = [];
+const router = (0, express_1.Router)();
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    res.status(200).json({ todos: todos });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    }
+    res.status(404).json({ message: 'cannot find todo for this id.' });
+});
+exports.default = router;
